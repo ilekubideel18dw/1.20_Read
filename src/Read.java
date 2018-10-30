06b43e13fb3c9ae07f29f5f7c0bc11c80a4b31c4
ik013043z1 <ik013043z1@H013043.NET>
2018-10-30 13:48:21 +0100

    4.0

--- src/Read.java
+++ src/Read.java
@@ -5,22 +5,29 @@ import java.util.Scanner;
 public class Read {
 	public static void main(String[] args) {
 		
+		// Here, I define user only can enter one argument, not more than one or anyone.
 		if (args.length > 1 ) {
-			System.out.println("You enter more than one parameter. Bye!");
+			System.out.println("You enter more than one argument. Bye!");
 		}
 		else if (args.length < 1) {
-			System.out.println("You don't enter a parameter. Bye!");
+			System.out.println("You don't enter a argument. Bye!");
 		}
-		else {			
+		else {
+			
+			// Here, I create a variable to save the file we define in the argument.
 			try{
 				File file = new File(args[0]);
 				Scanner sc = new Scanner(file);
+				
+				// Here, I create a variable to read the text there is inside the file.
 				while(sc.hasNextLine()){
 					String line = sc.nextLine();
 					System.out.println(line);
 				}
 				 
 			}
+			
+			// If the program don't found any file with the name user put, is going to say "File not found...".
 			catch(FileNotFoundException e){
 				System.out.println("File not found...");
 			}
